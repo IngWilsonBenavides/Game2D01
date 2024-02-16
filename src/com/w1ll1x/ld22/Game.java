b1cf345945d06f21dc2bc7af50745929f41e118b
ingWilsonBenavides <ing.wilson.benavides@gmail.com>
2024-02-16 08:31:42 -0500

    05 now lastTime frames random pixels 27:18

--- src/com/w1ll1x/ld22/Game.java
+++ src/com/w1ll1x/ld22/Game.java
@@ -20,6 +20,7 @@ public class Game extends Canvas implements Runnable {
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	private boolean running = false;
+	private int tickCount;
 
 	public void start() {
 		running = true;
@@ -31,18 +32,34 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime();
+		double unprocessed = 0;
+		double nsPerTick = 1000000000.0 / 60.0;
+		int frames = 0;
+		long lastTimer1 = System.currentTimeMillis();
+
 		while (running) {
-			runStep();
+			long now = System.nanoTime();
+			unprocessed += (now - lastTime) / nsPerTick;
+			lastTime = now;
+			System.out.println(unprocessed);
+			while (unprocessed >= 1) {
+				tick();
+				unprocessed -= 1;
+			}
+			{
+				frames++;
+				render();
+			}
+
+			if (System.currentTimeMillis() - lastTimer1 > 1000) {
+				lastTimer1 += 1000;
+				System.out.println(frames + " fps");
+				frames = 0;
+			}
 		}
 	}
 
-	private void runStep() {
-		tick();
-		render();
-	}
-
-	int tickCount;
-	
 	public void tick() {
 		tickCount++;
 	}
@@ -56,7 +73,6 @@ public class Game extends Canvas implements Runnable {
 
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = i + tickCount;
-//			pixels[i] = 1000;
 		}
 
 		Graphics g = bs.getDrawGraphics();
