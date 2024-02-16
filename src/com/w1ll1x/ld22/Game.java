d4d4455ce4dca93ea4c204ce382faba092dcf481
ingWilsonBenavides <ing.wilson.benavides@gmail.com>
2024-02-16 09:37:47 -0500

    06 frames per second 40:05

--- src/com/w1ll1x/ld22/Game.java
+++ src/com/w1ll1x/ld22/Game.java
@@ -36,26 +36,37 @@ public class Game extends Canvas implements Runnable {
 		double unprocessed = 0;
 		double nsPerTick = 1000000000.0 / 60.0;
 		int frames = 0;
+		int ticks = 0;
 		long lastTimer1 = System.currentTimeMillis();
 
 		while (running) {
 			long now = System.nanoTime();
 			unprocessed += (now - lastTime) / nsPerTick;
 			lastTime = now;
-			System.out.println(unprocessed);
+			boolean shouldRender = true;
+
 			while (unprocessed >= 1) {
+				ticks++;
 				tick();
 				unprocessed -= 1;
+				shouldRender = true;
 			}
-			{
+			try {
+
+				Thread.sleep(2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (shouldRender) {
 				frames++;
 				render();
 			}
 
 			if (System.currentTimeMillis() - lastTimer1 > 1000) {
 				lastTimer1 += 1000;
-				System.out.println(frames + " fps");
+				System.out.println(ticks + " ticks, " + frames + " fps");
 				frames = 0;
+				ticks = 0;
 			}
 		}
 	}
