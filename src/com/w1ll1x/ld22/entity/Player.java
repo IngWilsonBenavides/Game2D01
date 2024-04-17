32ded2cb8ee6e9f706bad6642195f7bf17a6dc95
w1ll1x <ing.wilson.benavides@gmail.com>
2024-04-17 17:00:28 -0500

    15 testing attack when key C is pressed  2:46:23

--- src/com/w1ll1x/ld22/entity/Player.java
+++ src/com/w1ll1x/ld22/entity/Player.java
@@ -7,7 +7,9 @@ import com.w1ll1x.ld22.gfx.Screen;
 public class Player extends Mob {
 
 	private InputHandler input;
-	
+	private boolean wasAttacking;
+	private int attackTime, attackDir;
+
 	public Player(InputHandler input) {
 		this.input = input;
 		x = 16;
@@ -27,6 +29,18 @@ public class Player extends Mob {
 		if (input.right)
 			xa++;
 		move(xa, ya);
+
+		if (input.attack) {
+			if (!wasAttacking) {
+				attackDir = dir;
+				attackTime = 5;
+				wasAttacking = true;
+			}
+		} else {
+			wasAttacking = false;
+		}
+		if (attackTime > 0)
+			attackTime--;
 	}
 
 	public void render(Screen screen) {
@@ -50,27 +64,27 @@ public class Player extends Mob {
 
 		int xo = x - 8;
 		int yo = y - 11;
-		
-		if (dir == 1) {			
-			screen.render(xo + 0, yo - 2, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 0);
-			screen.render(xo + 8, yo - 2, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 1);
+
+		if (attackTime > 0 && attackDir == 1) {
+			screen.render(xo + 0, yo - 4, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 0);
+			screen.render(xo + 8, yo - 4, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 1);
 		}
-		
+
 		screen.render(xo + 8 * flip1, yo + 0, xt + yt * 32, Color.get(-1, 100, 220, 532), flip1);
 		screen.render(xo + 8 - 8 * flip1, yo + 0, xt + 1 + yt * 32, Color.get(-1, 100, 220, 532), flip1);
 		screen.render(xo + 8 * flip2, yo + 8, xt + (yt + 1) * 32, Color.get(-1, 100, 220, 532), flip2);
 		screen.render(xo + 8 - 8 * flip2, yo + 8, xt + 1 + (yt + 1) * 32, Color.get(-1, 100, 220, 532), flip2);
-		if (dir == 2) {			
-			screen.render(xo - 2, yo, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 1);
-			screen.render(xo - 2, yo + 8, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 3);
+		if (attackTime > 0 && attackDir == 2) {
+			screen.render(xo - 4, yo, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 1);
+			screen.render(xo - 4, yo + 8, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 3);
 		}
-		if (dir == 3) {			
-			screen.render(xo + 8 + 2, yo, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 0);
-			screen.render(xo + 8 + 2, yo + 8, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 2);
+		if (attackTime > 0 && attackDir == 3) {
+			screen.render(xo + 8 + 4, yo, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 0);
+			screen.render(xo + 8 + 4, yo + 8, 7 + 13 * 32, Color.get(-1, 555, 555, 555), 2);
 		}
-		if (dir == 0) {			
-			screen.render(xo + 0, yo + 8 + 2, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 2);
-			screen.render(xo + 8, yo + 8 + 2, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 3);
+		if (attackTime > 0 && attackDir == 0) {
+			screen.render(xo + 0, yo + 8 + 4, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 2);
+			screen.render(xo + 8, yo + 8 + 4, 6 + 13 * 32, Color.get(-1, 555, 555, 555), 3);
 		}
 	}
 
