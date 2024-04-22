b939bf3cbad7e6c177d2649af60df7fa4f77c521
w1ll1x <ing.wilson.benavides@gmail.com>
2024-04-21 20:42:22 -0500

    22 progress in noise map generation 3:53:25

--- src/com/w1ll1x/ld22/level/levelgen/NoiseMap.java
+++ src/com/w1ll1x/ld22/level/levelgen/NoiseMap.java
@@ -8,18 +8,24 @@ import javax.swing.JOptionPane;
 
 public class NoiseMap {
 
+	private static final Random random = new Random();
 	public double[] values;
 	private int w, h;
 
-	public NoiseMap(int w, int h) {
+	public NoiseMap(int w, int h, int featureSize) {
 		this.w = w;
 		this.h = h;
 
 		values = new double[w * h];
 
-		Random random = new Random();
+		for (int y = 0; y < w; y += featureSize) {
+			for (int x = 0; x < w; x += featureSize) {
+				setSample(x, y, random.nextFloat() * 2 - 1);
+			}
+		}
 
-		int stepSize = w;
+		int stepSize = featureSize;
+		double scale = 1.0 / w;
 		do {
 			int halfStep = stepSize / 2;
 			for (int y = 0; y < w; y += stepSize) {
@@ -28,8 +34,7 @@ public class NoiseMap {
 					double b = sample(x + stepSize, y);
 					double c = sample(x, y + stepSize);
 					double d = sample(x + stepSize, y + stepSize);
-					double scale = 1.0 / w;
-	
+
 					double e = (a + b + c + d) / 4.0 + (random.nextFloat() * 2 - 1) * stepSize * scale;
 					setSample(x + halfStep, y + halfStep, e);
 				}
@@ -42,8 +47,7 @@ public class NoiseMap {
 					double d = sample(x + halfStep, y + halfStep);
 					double e = sample(x + halfStep, y - halfStep);
 					double f = sample(x - halfStep, y + halfStep);
-					double scale = 1.0 / w;
-					
+
 					double H = (a + b + d + e) / 4.0 + (random.nextFloat() * 2 - 1) * stepSize * scale;
 					double g = (a + b + c + f) / 4.0 + (random.nextFloat() * 2 - 1) * stepSize * scale;
 					setSample(x + halfStep, y, H);
@@ -51,21 +55,41 @@ public class NoiseMap {
 				}
 			}
 			stepSize /= 2;
-		} while( stepSize > 1);
+			scale *= 1.4;
+		} while (stepSize > 1);
 	}
 
 	private double sample(int x, int y) {
 		return values[(x & (w - 1)) + (y & (h - 1)) * w];
 	}
-	
+
 	private void setSample(int x, int y, double value) {
 		values[(x & (w - 1)) + (y & (h - 1)) * w] = value;
 	}
-	
+
 	public static void main(String[] args) {
-		int w = 256;
-		int h = 256;
+		int w = 512;
+		int h = 512;
+		NoiseMap noise1 = new NoiseMap(w, h, 32);
+		NoiseMap noise2 = new NoiseMap(w, h, 64);
+
 		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		int[] pixels = new int[w * h];
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				int i = x + y * w;
+
+				double val = noise1.values[i] + noise2.values[i];
+
+				double xd = x / (w - 1.0) * 2 - 1;
+				double yd = y / (h - 1.0) * 2 - 1;
+				val = val + 1 - (xd * xd + yd * yd) * 4;
+
+				int br = val < 0 ? 0 : 255;
+				pixels[i] = br << 16 | br << 8 | br;
+			}
+		}
+		img.setRGB(0, 0, w, h, pixels, 0, w);
 		JOptionPane.showMessageDialog(null, null, "The image", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(img));
 	}
 }
