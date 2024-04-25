fe5068992348d3f39696b53d40ae46930e1c79da
w1ll1x <ing.wilson.benavides@gmail.com>
2024-04-24 19:22:10 -0500

    25 border transition render between water and grass 4:23:56

--- src/com/w1ll1x/ld22/level/tile/WaterTile.java
+++ src/com/w1ll1x/ld22/level/tile/WaterTile.java
@@ -13,13 +13,31 @@ public class WaterTile extends Tile {
 
 	public void render(Screen screen, Level level, int x, int y) {
 		int col = Color.get(4, 4, 335, 335);
-		screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
-		screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 0, 0, col, 0);
-		screen.render(x * 16 + 0, y * 16 + 8, 0, col, 0);
-		screen.render(x * 16 + 8, y * 16 + 8, 0, col, 0);
+		int transitionColor = Color.get(100, 4, 211, level.grassColor);
+
+		boolean u = level.getTile(x, y - 1) != this;
+		boolean d = level.getTile(x, y + 1) != this;
+		boolean l = level.getTile(x - 1, y) != this;
+		boolean r = level.getTile(x + 1, y) != this;
+
+		if (!u && !l) {
+			screen.render(x * 16 + 0, y * 16 + 0, 0, col, 0);
+		} else {
+			screen.render(x * 16 + 0, y * 16 + 0, (l ? 4 : 5) + (1 - (u ? 0 : 1)) * 32, transitionColor, 0);
+		}
+		if (!u && !r) {
+			screen.render(x * 16 + 8, y * 16 + 0, 0, col, 0);
+		} else {
+			screen.render(x * 16 + 0, y * 16 + 0, (r ? 6 : 5) + (1 - (u ? 0 : 1)) * 32, transitionColor, 0);
+		}
+		if (!d && !l) {
+			screen.render(x * 16 + 0, y * 16 + 8, 0, col, 0);
+		}
+		if (!d && !r) {
+			screen.render(x * 16 + 8, y * 16 + 8, 0, col, 0);
+		}
 	}
-	
+
 	public boolean mayPass(Level level, int x, int y, Entity e) {
 		return false;
 	}
